d95098c054a34bc3fc90be14f7d3f8e792bb1a37
phucthuan1st <phucthuan1st@gmail.com>
2022-07-04 16:52:36 +0700

    Initial Commit

---
+++ XMLParsingToSQLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XMLParsingToSQLite
+//
+//  Created by MacMini01 on 04/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
