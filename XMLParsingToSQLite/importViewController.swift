fe2c4db9dfbed00bd9c7630721ebd6c8ce0c740d
phucthuan1st <phucthuan1st@gmail.com>
2022-07-05 15:32:32 +0700

    load file from local, select multiple file
    now can load the file list from local /data folder, then display these
    file selection to a table view
    On workong: Navigation when press Import
    Issue: read file, SQLite, XML Parse (Parsing to an UI)

--- XMLParsingToSQLite/importViewController.swift
+++ XMLParsingToSQLite/importViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 class importViewController: UIViewController {
     
     @IBOutlet weak var fileTable: UITableView!
-    let test = ["Banana", "Apple", "Pear", "Durian", "Orange"]
+    
+    let fileManager = FileManager.default
+    var fileList = [String]()
+    var selectedList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         showDataFileOnStorage()
+        
     }
-}
 
-extension importViewController {
-    
+    @IBAction func didTapImport(_ sender: Any) {
+        print("import \(selectedList)")
+    }
 }
 
 extension importViewController : UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return test.count
+        return fileList.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,8 +40,7 @@ extension importViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = fileTable.dequeueReusableCell(withIdentifier: "fileCell", for: indexPath) as! FileCell
         
-        cell.fileName.text = test[indexPath.section]
-        cell.fileSize.text = "1"
+        cell.fileName.text = fileList[indexPath.section]
         cell.check.isHidden = true
         
         return cell
@@ -47,24 +49,78 @@ extension importViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath) as! FileCell
         cell.check.isHidden = false
+        
+        selectedList.append(cell.fileName.text!)
+        print(selectedList)
     }
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath) as! FileCell
         
         cell.check.isHidden = true
+        selectedList.removeAll(where: {$0 == cell.fileName.text})
+        print(selectedList)
     }
     
     func showDataFileOnStorage() {
+        getFileList(From: dataDir())
         fileTable.allowsMultipleSelection = true
         fileTable.dataSource = self
         fileTable.delegate = self
     }
 }
 
+extension importViewController {
+    func dataDir() -> String {
+        let dirPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        do {
+            let docsDir = dirPath[0].path
+
+            if !fileManager.changeCurrentDirectoryPath(docsDir) {
+                let alert = UIAlertController(title: "Warning", message: "Documents folder not exist", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            }
+            
+            let dataDir = dirPath[0].appendingPathComponent("data").path
+            
+            if !fileManager.changeCurrentDirectoryPath(dataDir) {
+                try fileManager.createDirectory(atPath: dataDir, withIntermediateDirectories: true, attributes: nil)
+            }
+            
+            return dataDir
+        }
+        catch {
+            let alert = UIAlertController(title: "Warning", message: "Error when try to load data or missing directory", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
+            return ""
+        }
+    }
+    
+    func getFileList(From dataDir:String) {
+        if dataDir == "" {
+            fileList = []
+        }
+        else {
+            do {
+                let dataQueue = DispatchQueue(label: "public.load.fileList")
+                try dataQueue.sync {
+                    fileList = try fileManager.contentsOfDirectory(atPath: dataDir)
+                }
+                
+                fileList = fileList.filter {$0.contains(".xml")}
+            }
+            catch {
+                let alert = UIAlertController(title: "Warning", message: "Error when try to load data", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            }
+        }
+    }
+}
+
 class FileCell : UITableViewCell {
     @IBOutlet weak var check: UIImageView!
-    @IBOutlet weak var fileSize: UILabel!
     @IBOutlet weak var fileName: UILabel!
 }
 
