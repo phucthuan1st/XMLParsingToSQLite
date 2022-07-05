5d270c5172b2b2ba28d0e85401c159193b54e877
phucthuan1st <phucthuan1st@gmail.com>
2022-07-05 10:04:50 +0700

    import screen build

---
+++ XMLParsingToSQLite/database.swift
@@ -0,0 +1,9 @@
+//
+//  database.swift
+//  XMLParsingToSQLite
+//
+//  Created by MacMini01 on 05/07/2022.
+//
+
+import Foundation
+
--- XMLParsingToSQLite/importViewController.swift
+++ XMLParsingToSQLite/importViewController.swift
@@ -9,16 +9,61 @@ import UIKit
 
 class importViewController: UIViewController {
     
+    @IBOutlet weak var fileTable: UITableView!
+    let test = ["Banana", "Apple", "Pear", "Durian", "Orange"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        showDataFileOnStorage()
     }
-
-
 }
 
-class fileCell : UITableViewCell {
+extension importViewController {
+    
+}
+
+extension importViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return test.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = fileTable.dequeueReusableCell(withIdentifier: "fileCell", for: indexPath) as! FileCell
+        
+        cell.fileName.text = test[indexPath.section]
+        cell.fileSize.text = "1"
+        cell.check.isHidden = true
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! FileCell
+        cell.check.isHidden = false
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! FileCell
+        
+        cell.check.isHidden = true
+    }
+    
+    func showDataFileOnStorage() {
+        fileTable.allowsMultipleSelection = true
+        fileTable.dataSource = self
+        fileTable.delegate = self
+    }
+}
+
+class FileCell : UITableViewCell {
+    @IBOutlet weak var check: UIImageView!
     @IBOutlet weak var fileSize: UILabel!
     @IBOutlet weak var fileName: UILabel!
 }
