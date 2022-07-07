0e361c00e19d129e983ba822147422855d2e440b
phucthuan1st <phucthuan1st@gmail.com>
2022-07-07 15:36:16 +0700

    change in loadingView
    - loading by some way can copy file (that in previous version cannot)

--- XMLParsingToSQLite/loadingViewController.swift
+++ XMLParsingToSQLite/loadingViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class loadingViewController: UIViewController {
     
+    @IBOutlet weak var progressingFile: UILabel!
     @IBOutlet weak var progressingBar: UIProgressView!
     
     var fileList = [String]()
@@ -16,11 +17,7 @@ class loadingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.progressingBar.setProgress(0.0, animated: false)
-        
-        DispatchQueue.global().async {
-            self.moveFileToOfficial()
-        }
+        moveFileToOfficial()
     }
 }
 
@@ -28,37 +25,61 @@ extension loadingViewController {
     
     //MARK: move to FileHelper later
     func moveFileToOfficial() {
-        let fh = FileHelper()
+        let fileManager = FileManager.default
+        let dirPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        let docsDir = dirPath[0].path
+        if !fileManager.changeCurrentDirectoryPath(docsDir) {
+            cancelAlert(message: "Documents folder not exist")
+            self.navigationController?.popViewController(animated: true)
+        }
+
         do {
-            let dataDir = fh.pathToDir("data")
-            let officialDataDir = fh.pathToDir("official-data")
             
+            let officialDataDir = dirPath[0].appendingPathComponent("official-data").path
+            let dataDir = dirPath[0].appendingPathComponent("data").path
+            
+            
+            if !fileManager.changeCurrentDirectoryPath(officialDataDir) {
+                try fileManager.createDirectory(atPath: officialDataDir, withIntermediateDirectories: true, attributes: nil)
+            }
+            
+            progressingBar.setProgress(0.0, animated: true)
+            self.progressingFile?.text = fileList.first
             var complete:Float = 0.0
             
             for file in self.fileList {
-                print(file)
-                let fromFileDir = dataDir + "/" + file
-                let toFileDir = officialDataDir + "/" + file
                 
-                print(fromFileDir)
-                print(toFileDir)
-                try fh.copyItem(from: fromFileDir, to: toFileDir)
-                complete += 1.0 / Float(fileList.count)
+                let fromDir = dataDir + "/" + file
+                let toDir = officialDataDir + "/" + file
                 
+                complete += self.copyItem(from: fromDir, to: toDir) / Float(self.fileList.count)
                 DispatchQueue.main.async { [weak self] in
+                    print("File: \(file) imported")
                     self?.progressingBar.setProgress(complete, animated: true)
                 }
             }
         }
         catch {
+            cancelAlert(message: error.localizedDescription)
+        }
+    }
+    
+    func copyItem(from:String, to:String) -> Float {
+        do {
+            try FileManager.default.copyItem(atPath: from, toPath: to)
+        }
+        catch {
             print(error.localizedDescription)
         }
+        
+        return 1
     }
     //-----------------------------------------
     
     func cancelAlert(message:String) {
         let alert = UIAlertController(title: "Warning", message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(alert, animated: true)
     }
 }
