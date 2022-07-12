e19f9ef73ca6b76d39353dfcf5d161db4af819ca
phucthuan1st <phucthuan1st@gmail.com>
2022-07-12 15:12:58 +0700

    XML Parsing, write data to sqlite, load sqlite to data
    complete parser xml file
    write data to sqlite
    load saved data on sqlite to storing var
    
    Issue:
    - Threading, background
    - Deadlock
    - Don't show final result when parse large xml file

--- XMLParsingToSQLite/DBManager.swift
+++ XMLParsingToSQLite/DBManager.swift
@@ -1,6 +1,11 @@
+struct Record {
+    var instanceID:String?
+    var instanceName:String?
+}
+
 class DBManager: NSObject {
     static let shared: DBManager = DBManager()
-    let databaseFileName = "xmlDatabase.sqlite"
+    let databaseFileName = "xmlDatabase.db"
     var pathToDatabase: String!
     var database: FMDatabase!
     
@@ -12,20 +17,104 @@ class DBManager: NSObject {
     }
     
     func createDatabase() -> Bool {
-        //MARK: change later
-        let created = false
+        var created = false
      
         if !FileManager.default.fileExists(atPath: pathToDatabase) {
             database = FMDatabase(path: pathToDatabase!)
-        }
-        
-        if database != nil {
-            if !database.open() {
-                print("Could not open the database.")
+     
+            if database != nil {
+                // Open the database.
+                if database.open() {
+                    let createTableQuery = "create table XMLInstance (instanceID text not null primary key, instanceName text)"
+     
+                    do {
+                        try database.executeUpdate(createTableQuery, values: nil)
+                        created = true
+                    }
+                    catch {
+                        print("Could not create table.")
+                        print(error.localizedDescription)
+                    }
+     
+                    // At the end close the database.
+                    database.close()
+                }
+                else {
+                    print("Could not open the database.")
+                }
             }
         }
+     
         return created
     }
     
+    func openDatabase() -> Bool {
+        if database == nil {
+            if FileManager.default.fileExists(atPath: pathToDatabase) {
+                database = FMDatabase(path: pathToDatabase)
+            }
+            else {
+                if createDatabase() {
+                    print("New database created")
+                }
+            }
+        }
+        
+        if database != nil {
+            return database.open()
+        }
+        
+        return false
+    }
     
+    func insertRecord(record:Record) -> Bool {
+        let query = "INSERT INTO XMLInstance VALUES (\"\(record.instanceID!)\", \"\(record.instanceName!)\")"
+        if !openDatabase() {
+            print("cannot open database")
+            database.close()
+            return false
+        }
+        else {
+            do {
+                try database.executeUpdate(query, values: nil)
+                database.close()
+                return true
+            }
+            catch {
+                print("cannot insert \(record): - \(error.localizedDescription)")
+                database.close()
+                return false
+            }
+        }
+    }
+    
+    func loadRecord() -> [Record] {
+        if openDatabase() {
+            let query = "select * from XMLInstance"
+        
+            do {
+                
+                let result = try database.executeQuery(query, values: nil)
+                
+                var xmlRecord = [Record]()
+                
+                while result.next() {
+                    let record = Record(instanceID: result.string(forColumn: "instanceID"),
+                                        instanceName: result.string(forColumn: "instanceName"))
+                    
+                    xmlRecord.append(record)
+                }
+                
+                database.close()
+                return xmlRecord
+            }
+            catch {
+                print(error.localizedDescription)
+                database.close()
+                return [Record]()
+            }
+        }
+        
+        return [Record]()
+    }
 }
--- XMLParsingToSQLite/loadingViewController.swift
+++ XMLParsingToSQLite/loadingViewController.swift
@@ -7,47 +7,39 @@
 
 import UIKit
 
-struct XMLInfo {
-    var instanceID:String?
-    var instanceName:String?
-}
-
 class loadingViewController: UIViewController {
     
     @IBOutlet weak var progressingBar: UIProgressView!
     
+    var elementName:String?
     var fileList = [String]()
+    
+    var record:Record?
+    var records = [Record]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         progressingBar.setProgress(0.0, animated: true)
-        moveFileToOfficial()
-    }
-}
-
-extension loadingViewController {
-    
-    //MARK: move to FileHelper later
-    func moveFileToOfficial() {
-        DispatchQueue.global().async {
-
+        
+        DispatchQueue.global(qos: .userInitiated).async {
             do {
-                let fh = FileHelper.shared
-                
-                if !fh.existDirectory(Path: fh.pathToDir("official-data")) {
-                    try fh.createDirectory(Path: fh.pathToDir("official-data"))
-                }
-
-                var complete:Float = 0.0
-                    
+                try self.moveFileToOfficial()
                 for file in self.fileList {
-                    try fh.copyItem(from: "data/" + file, to: "official-data/" + file)
-                    complete += 1.0 / Float(self.fileList.count)
+                    try self.handleForXMLInstace(path: file)
+                }
+                
+                let progress = 1.0 + Float(self.fileList.count)
+                DispatchQueue.main.async {
+                    self.progressingBar.setProgress(progress, animated: true)
                     
-                    DispatchQueue.main.async { [weak self] in
-                        print("File: \(file) imported")
-                        self?.progressingBar.setProgress(complete, animated: true)
+                    if self.progressingBar.progress == 1.0 {
+                        let xmlViewController = self.storyboard?.instantiateViewController(withIdentifier: "xmlViewController") as! xmlViewController
+                        
+                        //xmlViewController.records = self.records
+                        
+                        self.navigationController?.popViewController(animated: false)
+                        self.navigationController?.pushViewController(xmlViewController, animated: true)
                     }
                 }
                 
@@ -58,18 +50,82 @@ extension loadingViewController {
         }
     }
     
-    func storeXMLValue() {
-        
-    }
-    
-    func writeXMLInfoToDataBase() {
-        
-    }
-    //-----------------------------------------
-    
     func Alert(message:String) {
         let alert = UIAlertController(title: "Warning", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .default))
         present(alert, animated: true)
     }
+}
+
+extension loadingViewController {
+    
+    //MARK: move to FileHelper later
+    func moveFileToOfficial() throws {
+
+        let fh = FileHelper.shared
+        
+        if !fh.existDirectory(Path: fh.pathToDir("official-data")) {
+            try fh.createDirectory(Path: fh.pathToDir("official-data"))
+        }
+            
+        for file in self.fileList {
+            try fh.copyItem(from: "data/" + file, to: "official-data/" + file)
+        }
+
+    }
+}
+
+extension loadingViewController : XMLParserDelegate {
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == "instanceID" {
+            record = Record(instanceID: "", instanceName: "")
+            self.elementName = elementName
+        }
+        if elementName == "instanceName" {
+            if record == nil {
+                record = Record(instanceID: "Unknown ID", instanceName: "")
+            }
+            self.elementName = elementName
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "instanceID" {
+        }
+        if elementName == "instanceName" {
+            records.append(record!)
+            if DBManager.shared.insertRecord(record: record!) {
+                print("Insert record successfully")
+            }
+            record = nil
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
+        if (!string.isEmpty) {
+
+            if self.elementName == "instanceID" {
+                record?.instanceID! += string
+            }
+            if self.elementName == "instanceName" {
+                record?.instanceName! += string
+            }
+        }
+    }
+}
+
+extension loadingViewController {
+    func handleForXMLInstace(path:String) throws {
+        let filePath = FileHelper.shared.pathToDir("official-data") + "/" + path
+        let inputStream = InputStream(fileAtPath: filePath)
+        let xmlParser = XMLParser(stream: inputStream!)
+        
+        xmlParser.delegate = self
+        
+        xmlParser.parse()
+        
+        inputStream?.close()
+    }
+    
 }
